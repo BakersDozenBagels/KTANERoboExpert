056da18364c13a1ce44f1e29647413e7ee96a6c5
BakersDozenBagels <bakersdozenbagelsbergman@gmail.com>
2024-07-12 14:11:35 -0400

    Add Venting Gas

---
+++ KTANERoboExpert/Modules/VentingGas.cs
@@ -0,0 +1,14 @@
+﻿using System.Speech.Recognition;
+
+namespace KTANERoboExpert.Modules;
+
+public class VentingGas : RoboExpertModule
+{
+    public override string Name => "Venting Gas";
+    public override string Help => "Vent gas | Detonate";
+    private Grammar? _grammar;
+    public override Grammar Grammar => _grammar ??= new(new Choices("vent gas", "detonate"));
+
+    public override void ProcessCommand(string command) =>
+        Speak(command == "detonate" ? "Press no" : "Press yes");
+}
