cb1e1e8bdaec49c494a27bc28f1a65fd5b016a38
BakersDozenBagels <bakersdozenbagelsbergman@gmail.com>
2025-12-15 12:24:56 -0500

    Add Simpleton

---
+++ KTANERoboExpert/Modules/Simpleton.cs
@@ -0,0 +1,20 @@
+﻿using System.Diagnostics;
+using System.Speech.Recognition;
+
+namespace KTANERoboExpert.Modules;
+
+public class Simpleton : RoboExpertModule
+{
+    public override string Name => "Simpleton";
+    public override string Help => "";
+    private Grammar? _grammar;
+    public override Grammar Grammar => _grammar ??= new(new GrammarBuilder("unused"));
+
+    public override void ProcessCommand(string command) => throw new UnreachableException();
+
+    public override void Select()
+    {
+        Speak("Push the button.");
+        ExitSubmenu();
+    }
+}
