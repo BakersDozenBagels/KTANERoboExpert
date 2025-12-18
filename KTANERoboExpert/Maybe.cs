116e1904262224fc80eb7676662fc819bcd2c184
BakersDozenBagels <bakersdozenbagelsbergman@gmail.com>
2025-12-17 19:01:27 -0500

    Add Switches

--- KTANERoboExpert/Maybe.cs
+++ KTANERoboExpert/Maybe.cs
@@ -7,7 +7,7 @@ namespace KTANERoboExpert;
 /// Represents an item which may or may not be present.
 /// </summary>
 /// <typeparam name="T">The type of item</typeparam>
-public readonly struct Maybe<T> : IEnumerable<T>, IOrderedEnumerable<T>, IEquatable<Maybe<T>>
+public readonly struct Maybe<T> : IEnumerable<T>, IOrderedEnumerable<T>, IEquatable<Maybe<T>> where T : notnull
 {
     /// <summary>
     /// <see langword="true"/> when the item exists, <see langword="false"/> otherwise.
@@ -49,8 +49,10 @@ public readonly struct Maybe<T> : IEnumerable<T>, IOrderedEnumerable<T>, IEquata
 
     public static implicit operator Maybe<T>(T item) => new(item);
 
-    public Maybe<U> Map<U>(Func<T, U> map) => Exists ? map(Item) : new Maybe<U>();
-    public Maybe<U> FlatMap<U>(Func<T, Maybe<U>> map) => Exists ? map(Item) : new Maybe<U>();
+    public Maybe<T> OfNullable(T? item) => item is null ? default(Maybe<T>) : item!;
+
+    public Maybe<U> Map<U>(Func<T, U> map) where U : notnull => Exists ? map(Item) : default(Maybe<U>);
+    public Maybe<U> FlatMap<U>(Func<T, Maybe<U>> map) where U : notnull => Exists ? map(Item) : default;
 
     public T OrElse(T value) => Exists ? Item : value;
 
@@ -59,4 +61,6 @@ public readonly struct Maybe<T> : IEnumerable<T>, IOrderedEnumerable<T>, IEquata
     public static bool operator ==(Maybe<T> left, Maybe<T> right) => left.Equals(right);
     public static bool operator !=(Maybe<T> left, Maybe<T> right) => !(left == right);
     public override int GetHashCode() => HashCode.Combine(Exists, Item);
+
+    public override string ToString() => Exists ? $"Some({Item})" : "None";
 }
---
+++ KTANERoboExpert/Modules/Switches.cs
@@ -0,0 +1,57 @@
+﻿using KTANERoboExpert.Uncertain;
+using System.Diagnostics;
+using System.Speech.Recognition;
+using System.Xml.Linq;
+
+namespace KTANERoboExpert.Modules;
+
+public class Switches : RoboExpertModule
+{
+    public override string Name => "Switches";
+    public override string Help => "Up Up Up Up Up to Down Down Down Down Down";
+    private Grammar? _grammar;
+    public override Grammar Grammar => _grammar ??= new(new GrammarBuilder(new Choices("up", "down"), 5, 5) + "to" + new GrammarBuilder(new Choices("up", "down"), 5, 5));
+
+    public override void ProcessCommand(string command)
+    {
+        var parts = command.Split();
+        var sol = Solve(ToInt(parts[..5]), ToInt(parts[6..]));
+        if (sol is [])
+        {
+            Speak("Pardon?");
+            return;
+        }
+        Speak(sol.Select(x => (5 - x).ToString()).Conjoin());
+        ExitSubmenu();
+        Solve();
+    }
+
+    private static int ToInt(string[] s) => s.Select((s, i) => s == "up" ? 1 << (4 - i) : 0).Sum();
+
+    private static int[] Solve(int start, int end)
+    {
+        if (_illegal.Contains(start) || _illegal.Contains(end) || start == end)
+            return [];
+
+        HashSet<int> done = [start];
+        Queue<(int, int[])> todo = new([(start, [])]);
+
+        while (true)
+        {
+            var cur = todo.Dequeue();
+            for (int i = 0; i < 5; i++)
+            {
+                var next = cur.Item1 ^ (1 << i);
+                if (next == end)
+                    return [.. cur.Item2, i];
+                if (!_illegal.Contains(next) && !done.Contains(next))
+                {
+                    done.Add(next);
+                    todo.Enqueue((next, [.. cur.Item2, i]));
+                }
+            }
+        }
+    }
+
+    private static readonly HashSet<int> _illegal = [0b00100, 0b01011, 0b01111, 0b10010, 0b10011, 0b10111, 0b11000, 0b11010, 0b11100, 0b11110];
+}
